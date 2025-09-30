7b2367461acb914921a95784f947bdced14f5cc8
Ladislav Foldyna <foldyna@gmail.com>
2025-09-30 14:42:53 +0200

    ADIF 3.1.6: Added new Modes FSK and MTONE and their submodes

---
+++ res/sql/migration_035.sql
@@ -0,0 +1,2 @@
+INSERT INTO modes (name, submodes, rprt, dxcc, enabled) VALUES ('FSK', '["SCAMP_FAST", "SCAMP_SLOW", "SCAMP_VSLOW"]', NULL, 'DIGITAL', 0);
+INSERT INTO modes (name, submodes, rprt, dxcc, enabled) VALUES ('MTONE', '["SCAMP_OO", "SCAMP_OO_SLW"]', NULL, 'DIGITAL', 0);
